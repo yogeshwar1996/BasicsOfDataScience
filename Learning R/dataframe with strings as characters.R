2cd324d4da251880abef7fc823a8b9add3f238e6
yogeshwar1996 <yogeshwartrehan@gmail.com>
2020-04-18 14:06:12 +0530

    data frame

---
+++ Learning R/dataframe with strings as characters.R
@@ -0,0 +1,7 @@
+# creating a data frame with stringAsFactors = FALSE
+grades <- data.frame(names = c("John", "Juan", "Jean", "Yao"), 
+                     exam_1 = c(95, 80, 90, 85), 
+                     exam_2 = c(90, 85, 85, 90),
+                     stringsAsFactors = FALSE)
+class(grades)
+class(grades$names)
