9728776dda328399f24882ebc7fb9402056e57c3
yogeshwar1996 <yogeshwartrehan@gmail.com>
2020-05-02 12:33:57 +0530

    shift labels with nudge modify point size

--- Data visualization/scatter ggplot with layers.R
+++ Data visualization/scatter ggplot with layers.R
@@ -10,10 +10,11 @@ murders %>% ggplot() +  geom_point(aes(x = population/10^6, y = total))
 p <- ggplot(data = murders)
 p + geom_point(aes(population/10^6, total))
 
-# add text layer to scatterplot
-# in the following aes accesses population, total, abb from the data ggplot has been initiated with
-p + geom_point(aes(population/10^6, total)) +
-  geom_text(aes(population/10^6, total, label = abb))
+# add text layer to scatterplot, this will add labels to each point
+# labels can be shifted along x and y axis using nudge_x and nudge_y
+# size of points can also be changed
+p + geom_point(aes(population/10^6, total), size=0.5) +
+  geom_text(aes(population/10^6, total, label = abb), nudge_x = 1.5)
 
 p + geom_hex(aes(population/10^6, total)) +
-  geom_text(aes(population/10^6, total, label = abb))
+  geom_text(aes(population/10^6, total, label = abb), nudge_x = 1.5)
