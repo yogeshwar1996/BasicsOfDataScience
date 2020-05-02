31df800ef8b95c9cf051c41f6809f8da80e7f996
yogeshwar1996 <yogeshwartrehan@gmail.com>
2020-05-02 13:11:17 +0530

    line along with points, color by region

---
+++ Data visualization/scale x and y axis.R
@@ -0,0 +1,62 @@
+# define p
+library(tidyverse)
+library(dslabs)
+data(murders)
+p <- murders %>% ggplot(aes(population/10^6, total, label = abb))
+
+# log base 10 scale the x-axis and y-axis
+p + geom_point(size = 3) +
+  geom_text(nudge_x = 0.05) +
+  scale_x_continuous(trans = "log10") +
+  scale_y_continuous(trans = "log10")
+
+# efficient log scaling of the axes
+p + geom_point(size = 3) +
+  geom_text(nudge_x = 0.075) +
+  scale_x_log10() +
+  scale_y_log10()
+
+#labels and title
+p + geom_point(size = 3) +
+  geom_text(nudge_x = 0.075) +
+  scale_x_log10() +
+  scale_y_log10() +
+  xlab("Population in millions (log scale)") +
+  ylab("Total number of murders (log scale)") +
+  ggtitle("US Gun Murders in 2010")
+
+
+# redefine p to be everything except the points layer
+p <- murders %>%
+  ggplot(aes(population/10^6, total, label = abb)) +
+  geom_text(nudge_x = 0.075) +
+  scale_x_log10() +
+  scale_y_log10() +
+  xlab("Population in millions (log scale)") +
+  ylab("Total number of murders (log scale)") +
+  ggtitle("US Gun Murders in 2010")
+
+# make all points blue
+p + geom_point(size = 3, color = "blue")
+
+# color points by region
+p + geom_point(aes(col = region), size = 3)
+
+# Add a line with average murder rate per million
+# define average murder rate
+r <- murders %>%
+  summarize(rate = sum(total) / sum(population) * 10^6) %>%
+  pull(rate)
+# Because if a state has a population x and it has the same murder
+# rate as the US, which is r, we simply multiply
+# r by x to get the total number of murders.
+# since y=rx slope =1, intercept at log scale = log10(r)
+# basic line with average murder rate for the country
+p + geom_point(aes(col = region), size = 3) +
+  geom_abline(intercept = log10(r))    # slope is default of 1
+
+# change line to dashed and dark grey, line under points, so render the line first and then the points
+# use lty to chnage line type
+p + 
+  geom_abline(intercept = log10(r), lty = 2, color = "darkgrey") +
+  geom_point(aes(col = region), size = 3)
