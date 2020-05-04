cc4aed99e5baa29c00ee3121919e14976758ac22
yogeshwar1996 <yogeshwartrehan@gmail.com>
2020-05-04 23:57:09 +0530

    use tidyverse

---
+++ Data visualization/working with gapminder.R
@@ -0,0 +1,16 @@
+# load and inspect gapminder data
+library(dslabs)
+data(gapminder)
+head(gapminder)
+
+# compare infant mortality in Sri Lanka and Turkey
+gapminder %>%
+  filter(year == 2015 & country %in% c("Sri Lanka", "Turkey")) %>%
+  select(country, infant_mortality)
+
+# basic scatterplot of life expectancy versus fertility
+# ds_theme_set()    # set plot theme
+gapminder %>% filter(year == 1962) %>%
+  ggplot(aes(fertility, life_expectancy, color=continent)) +
+  geom_point()
+
