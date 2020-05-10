ada377183634f480458992bc68d01325a43a8052
yogeshwar1996 <yogeshwartrehan@gmail.com>
2020-05-10 19:30:50 +0530

    imporve code and rename file

--- Data visualization/ggplot.R
+++ Data visualization/ggplot.R
@@ -1,9 +1,14 @@
 library(tidyverse)
+library(ggplot2)
+library(dslabs)
+data(murders)
 # intitialize the data component
 # Method1 
 ggplot(data=murders)
 # Method 2
 murders %>% ggplot()
 # assing plot object to a variable
-p = ggplot(data=murders)
+p = ggplot(data=murders, aes(x=murders$region)) +
+    geom_bar()
 # Simply printing the variable will render the plot
+p
