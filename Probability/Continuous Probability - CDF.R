58e7e857e344db014f6194c96f0662da514032df
yogeshwar1996 <yogeshwartrehan@gmail.com>
2020-05-23 21:05:10 +0530

    Discretization

--- Probability/Continuous Probability - CDF.R
+++ Probability/Continuous Probability - CDF.R
@@ -1,12 +1,22 @@
 library(tidyverse)
 library(dslabs)
-
-# The cumulative distribution function (CDF) is a distribution function for continuous data x that reports the proportion of the data below a for all values of a:
-  # F(a)=Pr(x≤a)
+library(dplyr)
+# The cumulative distribution function (CDF) is a distribution function for continuous data x
+# that reports the proportion of the data below a for all values of a:
+# F(a)=Pr(x≤a)
 # The CDF is the probability distribution function for continuous variables. 
 # For example, to determine the probability that a male student is taller than 70.5 inches 
 # given a vector of male heights x, we can use the CDF:
-    # Pr(x>70.5)=1−Pr(x≤70.5)=1−F(70.5)
+# Pr(x>70.5)=1−Pr(x≤70.5)=1−F(70.5)
 # The probability that an observation is in between two values a,b is F(b)−F(a).
 
+data(heights)
+View(heights)
+x <- heights %>% filter(sex=="Male") %>% pull(height)
+
+# Given a vector x, we can define a function for computing the CDF of x using:
+F <- function(a) {
+  mean(x <= a)
+  }
+1 - F(70)    # probability of male taller than 70 inches
 
