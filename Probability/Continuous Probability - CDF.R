41120c25b58e23c53c846bed89694d046df07f03
yogeshwar1996 <yogeshwartrehan@gmail.com>
2020-05-23 23:24:20 +0530

    rnorm

--- Probability/Continuous Probability - CDF.R
+++ Probability/Continuous Probability - CDF.R
@@ -20,3 +20,32 @@ F <- function(a) {
   }
 1 - F(70)    # probability of male taller than 70 inches
 
+# pnorm(a, avg, s) gives the value of the cumulative distribution function
+# F(a) for the normal distribution defined by average avg and standard deviation s.
+# We say that a random quantity is normally distributed with average avg and standard deviation s
+# if the approximation pnorm(a, avg, s) holds for all values of a.
+# We can estimate the probability that a male is taller than 70.5 inches using:
+1 - pnorm(70.5, mean(x), sd(x))
+
+
+# Discretization and the normal approximation
+table(x) #create a table with values in x as columns and frequency of x as the value
+prop.table(table(x)) #create a table with values in x as columns and proportion of x(i.e freq/total values) as the value
+# plot distribution of exact heights in data
+plot(prop.table(table(x)), xlab = "a = Height in inches", ylab = "Pr(x = a)")
+
+# probabilities in actual data over length 1, with range containing an integer
+mean(x <= 68.5) - mean(x <= 67.5)
+mean(x <= 69.5) - mean(x <= 68.5)
+mean(x <= 70.5) - mean(x <= 69.5)
+
+# probabilities in normal approximation match well
+pnorm(68.5, mean(x), sd(x)) - pnorm(67.5, mean(x), sd(x))
+pnorm(69.5, mean(x), sd(x)) - pnorm(68.5, mean(x), sd(x))
+pnorm(70.5, mean(x), sd(x)) - pnorm(69.5, mean(x), sd(x))
+
+# probabilities in actual data over range without an integer in b/w
+# does not match normal distributionheights %>% filter(sex=="Male") %>% sum(height)
+
+mean(x <= 70.9) - mean(x <= 70.1)
+pnorm(70.9, mean(x), sd(x)) - pnorm(70.1, mean(x), sd(x))
