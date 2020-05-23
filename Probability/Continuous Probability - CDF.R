1ad4b23673ec0693e9c1571443370265c142c67f
yogeshwar1996 <yogeshwartrehan@gmail.com>
2020-05-23 19:41:31 +0530

    Commulative distribution probability

---
+++ Probability/Continuous Probability - CDF.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(dslabs)
+
+# The cumulative distribution function (CDF) is a distribution function for continuous data x that reports the proportion of the data below a for all values of a:
+  # F(a)=Pr(x≤a)
+# The CDF is the probability distribution function for continuous variables. 
+# For example, to determine the probability that a male student is taller than 70.5 inches 
+# given a vector of male heights x, we can use the CDF:
+    # Pr(x>70.5)=1−Pr(x≤70.5)=1−F(70.5)
+# The probability that an observation is in between two values a,b is F(b)−F(a).
+
+
