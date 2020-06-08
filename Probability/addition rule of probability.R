24ac78cfb9e609d8b682328fbcf1e9bc2a345ffb
yogeshwar1996 <yogeshwartrehan@gmail.com>
2020-06-08 21:35:00 +0530

    Assessment

---
+++ Probability/addition rule of probability.R
@@ -0,0 +1,3 @@
+# The addition rule states that the probability of event A or event B happening
+# is the probability of event A plus the probability of event B minus the probability of both events A and B happening together.
+# Pr(A or B)=Pr(A)+Pr(B)−Pr(A and B)
