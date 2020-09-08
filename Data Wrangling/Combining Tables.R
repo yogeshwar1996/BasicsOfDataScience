5ffc737eb49490ad7a8f3e08ba43b3fa2dc4894e
yogeshwar1996 <yogeshwartrehan@gmail.com>
2020-09-08 19:12:06 +0530

    Create Combining Tables.R

---
+++ Data Wrangling/Combining Tables.R
@@ -0,0 +1,47 @@
+# Key points
+# 
+# The join functions in the dplyr package combine two tables such that matching rows are together.
+# left_join() only keeps rows that have information in the first table.
+# right_join() only keeps rows that have information in the second table.
+# inner_join() only keeps rows that have information in both tables.
+# full_join() keeps all rows from both tables.
+# semi_join() keeps the part of first table for which we have information in the second.
+# anti_join() keeps the elements of the first table for which there is no information in the second.
+# import US murders data
+library(tidyverse)
+library(ggrepel)
+library(dslabs)
+ds_theme_set()
+data(murders)
+head(murders)
+
+# import US election results data
+data(polls_us_election_2016)
+head(results_us_election_2016)
+identical(results_us_election_2016$state, murders$state)
+
+# join the murders table and US election results table
+tab <- left_join(murders, results_us_election_2016, by = "state")
+head(tab)
+
+# plot electoral votes versus population
+tab %>% ggplot(aes(population/10^6, electoral_votes, label = abb)) +
+  geom_point() +
+  geom_text_repel() + 
+  scale_x_continuous(trans = "log2") +
+  scale_y_continuous(trans = "log2") +
+  geom_smooth(method = "lm", se = FALSE)
+
+# make two smaller tables to demonstrate joins
+tab1 <- slice(murders, 1:6) %>% select(state, population)
+tab1
+tab2 <- slice(results_us_election_2016, c(1:3, 5, 7:8)) %>% select(state, electoral_votes)
+tab2
+
+# experiment with different joins
+left_join(tab1, tab2)
+tab1 %>% left_join(tab2)
+tab1 %>% right_join(tab2)
+inner_join(tab1, tab2)
+semi_join(tab1, tab2)
+anti_join(tab1, tab2)
